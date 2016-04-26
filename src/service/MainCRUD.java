cf6f03a329e74de8141e3cddded24a0a232640a9
sylwia211 <sylwia_karbowiak@op.pl>
2016-04-26 10:01:47 +0200

    Find and print: all points in the database, points that have distance of [field] less than [valeu], insert new point

--- src/service/MainCRUD.java
+++ src/service/MainCRUD.java
@@ -1,6 +1,5 @@
 package service;
 
-
 import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
@@ -18,12 +17,22 @@ import org.bson.Document;
  */
 public class MainCRUD {
 
+    static MongoDatabase db;
+
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient();
-        MongoDatabase db = mongoClient.getDatabase("testdb");
+        db = mongoClient.getDatabase("habitoudb");
 
-        FindIterable<Document> iterable = db.getCollection("restaurants").find(
-                new Document("borough", "Manhattan"));
+        findAll();
+        findDistance("atm", 6);
+        //insertPoint();
+
+    }
+
+    private static void findAll() {
+        System.out.println("== Find all");
+        FindIterable<Document> iterable = db.getCollection("points").find();
+        //new Document("borough", "Manhattan"));
 
         iterable.forEach(new Block<Document>() {
             @Override
@@ -31,6 +40,41 @@ public class MainCRUD {
                 System.out.println(document);
             }
         });
+    }
+
+    private static void findDistance(String point, int dist) {
+        System.out.println("== Find distance");
+        FindIterable<Document> iterable = db.getCollection("points").find(
+                new Document(point + ".dist", new Document("$lt", dist)));
+
+        iterable.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                System.out.println(document);
+            }
+        });
+    }
+
+    private static void insertPoint() {
+        db.getCollection("points").insertOne(
+                new Document("lat", -52.292034)
+                .append("long", 17.659409)
+                .append("atm",
+                        new Document()
+                        .append("dist", 8)
+                        .append("lat", -52.292034)
+                        .append("long", 17.659409))
+                .append("shop",
+                        new Document()
+                        .append("dist", 9)
+                        .append("lat", -52.292034)
+                        .append("long", 17.659409))
+                .append("transport",
+                        new Document()
+                        .append("dist", 10)
+                        .append("lat", -52.292034)
+                        .append("long", 17.659409)));
+
     }
 
 }
