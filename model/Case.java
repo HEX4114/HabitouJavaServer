ac4fee4db675d4bb414295288e5330c0c7f5069e
fcallet <francois.callet@insa-lyon.fr>
2016-04-26 08:51:48 +0200

    first files for model

---
+++ model/Case.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author François
+ */
+public class Case {
+    
+    private String id;
+ 
+    private Integer latitude;
+ 
+    private Integer longitude;
+    
+ 
+    public String getId() {
+        return id;
+    }
+ 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Integer latitude) {
+        this.latitude = latitude;
+    }
+
+    public Integer getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Integer longitude) {
+        this.longitude = longitude;
+    }
+    
+    
+    
+    
+}
