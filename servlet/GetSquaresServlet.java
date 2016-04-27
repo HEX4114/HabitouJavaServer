411fa0553e615bcf6e1530762f91b3c4e36539de
fcallet <francois.callet@insa-lyon.fr>
2016-04-27 08:39:35 +0200

    adding listener and servlet for Tomcat server

---
+++ servlet/GetSquaresServlet.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+/**
+ *
+ * @author François
+ */
+import java.io.IOException;
+import java.util.List;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+
+
+import com.mongodb.MongoClient;
+import model.Square;
+import dao.MongoDBSquareDao;
+ 
+@WebServlet("/getSquares")
+public class GetSquaresServlet extends HttpServlet {
+ 
+    private static final long serialVersionUID = -6554920927964049383L;
+ 
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        if (id == null || "".equals(id)) {
+            throw new ServletException("id missing for edit operation");
+        }
+        System.out.println("Person edit requested with id=" + id);
+        MongoClient mongo = (MongoClient) request.getServletContext()
+                .getAttribute("MONGO_CLIENT");
+        MongoDBSquareDao squareDAO = new MongoDBSquareDao(mongo);
+        List<Square> squares = squareDAO.readAllSquares();
+        request.setAttribute("squares", squares);
+ 
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(
+                "/persons.jsp");
+        rd.forward(request, response);
+    }
+}
